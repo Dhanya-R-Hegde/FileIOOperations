dbeefc324e3d8abb17128b52d2a704e3fb0df665
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-10-07 16:38:16 +0530

    [Dhanya R Hegde] : Added JSON Serialization and Deserialization

---
+++ JsonSerialization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Json.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace FileIOOperations
+{
+    internal class JsonSerialization
+    {
+        public void jsonSerialize()
+        {
+            BlogSites bsObj = new BlogSites()
+            {
+                Name = "Dhanya",
+                Description = "Welcome"
+            };
+            string jsonData = JsonConverter.SerializeObject(bsObj);
+
+            Console.WriteLine(jsonData);
+        }
+
+        public void jsonDeserialize()
+        {
+            string json = @"{
+                'Name' : 'Dhanya',
+                'Description' : 'Welcome'
+            }";
+            BlogSites bsObj = JsonConverter.DeserializeObject<BlogSites>(json);
+            Console.WriteLine(bsObj.Name);
+        }
+    }
+
+    [DataContract]
+    class BlogSites
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FileIOOperations;
+
+namespace FileIOOpertions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //IOOperations.FileExists();
+            //IOOperations.ReadAllLines();
+
+            StreamReaderOperations.WriteUsingStreamWriter();
+            StreamReaderOperations.ReadFromStreamReader();
+
+        }
+    }
+}
