d71d5011a66b7b93e7de7d273f13eb236b8f20e5
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-10-07 16:30:31 +0530

    [Dhanya R Hegde] : Added Stream Reader Operations

---
+++ StreamReaderOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperations
+{
+    public static class StreamReaderOperations
+    {
+        public static void ReadFromStreamReader()
+        {
+            String path = @"C:/Users/DHANYA R HEGDE/Desktop/dotNetBridgeLabs/FileIOOperations/FileIO.txt"; 
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+
+                while((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+            Console.ReadKey(); 
+        }
+
+        public static void WriteUsingStreamWriter()
+        {
+            String path = @"C:/Users/DHANYA R HEGDE/Desktop/dotNetBridgeLabs/FileIOOperations/FileIO.txt";
+
+            using(StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("\nHello World - .Net is awesome");
+                sw.Close();
+
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
