46602429f66edd4d7beadf78ea23cbb2b944f746
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-10-07 16:35:37 +0530

    [Dhanya R Hegde] : Added XML Serialization and Deserialization

---
+++ XMLSerialization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FileIOOperations
+{
+    internal class XMLSerialization
+    {
+        public void serializeXml()
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(OrderForm));
+            FileStream fileStream = new FileStream(@"", FileMode.Create);
+            OrderForm orderForm = new OrderForm();
+            DateTime dt = new DateTime(2013, 12, 31);
+            orderForm.orderDate = dt;
+            ser.Serialize(fileStream, orderForm);
+        }
+    }
+
+    public class OrderForm
+    {
+        public DateTime orderDate;
+    }
+}
